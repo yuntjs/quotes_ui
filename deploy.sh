08276cd9653f1d34d22268345d4a7687c61fa655
Tae Yun <yuntjs@gmail.com>
2017-07-11 17:17:13 -0400

    move around env

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env TAG=$TAG DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
+ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env TAG=0.1.$CIRCLE_BUILD_NUM DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
   rm -rf quotes_ui quotes_api quotes-provision-prod
   docker service update --image $TAG --force app_ui
 ENDSSH
