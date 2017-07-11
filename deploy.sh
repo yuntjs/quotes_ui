145431fdc7ff2f787525d635f2224a3fa04c6015
Tae Yun <yuntjs@gmail.com>
2017-07-11 17:47:46 -0400

    update with delauy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env TAG=0.1.$CIRCLE_BUILD_NUM DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
-  docker service update --image taejunyun/quotesui:$TAG --force app_ui
+  docker service update --image taejunyun/quotesui:$TAG --force --update-delay 3s app_ui 
 ENDSSH
