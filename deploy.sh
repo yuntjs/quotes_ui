81cb7e09bf6c7eadf611a19576cc426d09515149
Tae Yun <yuntjs@gmail.com>
2017-07-11 15:25:05 -0400

    change font of page

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,5 @@ ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amaz
   git clone https://github.com/yuntjs/quotes_ui.git
   git clone https://github.com/yuntjs/quotes_api.git
   git clone https://github.com/yuntjs/quotes-provision-prod.git
-  docker build -t="quotesui" ./quotes_ui
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker tag quotesui:latest taejunyun/quotesui
-  docker push taejunyun/quotesui
   docker stack deploy -c /home/docker/quotes-provision-prod/docker-compose.yml app
 ENDSSH
