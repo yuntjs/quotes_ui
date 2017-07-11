77b623496a75b80eed961b6c98090edafe156e8b
Tae Yun <yuntjs@gmail.com>
2017-07-11 01:58:23 -0400

    udpates deploy;

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com 'export DOCKER_USER="$DOCKER_USER" DOCKER_PASS="$DOCKER_PASS"; bash -s' <<'ENDSSH'
+ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
   sudo apk update
   sudo apk add git
   git clone https://github.com/yuntjs/quotes_ui.git
