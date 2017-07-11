ca1e17fd59e148cabcaddfd302274f89716baf2f
Tae Yun <yuntjs@gmail.com>
2017-07-11 01:04:21 -0400

    adjust deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com
-sudo apk update
-sudo apk add git
-git clone https://github.com/yuntjs/quotes_ui.git
-git clone https://github.com/yuntjs/quotes_api.git
-git clone https://github.com/yuntjs/quotes-provision-prod.git
-docker build -t="quotesui" ./quotes_ui
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker tag quotesui:latest taejunyun/quotesui
-docker push taejunyun/quotesui
-docker stack deploy -c quotes_api/docker-compose.yml
+ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com 'bash -s' <<'ENDSSH'
+  sudo apk update
+  sudo apk add git
+  git clone https://github.com/yuntjs/quotes_ui.git
+  git clone https://github.com/yuntjs/quotes_api.git
+  git clone https://github.com/yuntjs/quotes-provision-prod.git
+  docker build -t="quotesui" ./quotes_ui
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
+  docker tag quotesui:latest taejunyun/quotesui
+  docker push taejunyun/quotesui
+  docker stack deploy -c quotes_api/docker-compose.yml
+ENDSSH
