cc044375995d5deda7fded03aa97160077e0275e
Tae Yun <yuntjs@gmail.com>
2017-07-11 10:13:03 -0400

    update index and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
+  rm -rf quotes_ui quotes_api quotes-provision-prod
   sudo apk update
   sudo apk add git
   git clone https://github.com/yuntjs/quotes_ui.git
